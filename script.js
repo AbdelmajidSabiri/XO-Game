3e70391510de9a6ba9d74562b448d588004e803f
Abdelmajid Sabiri <abdlmajidsabiri26@gmail.com>
2024-06-29 16:15:27 +0100

    ADD winner fonction, to find the winner, and alse add timeout to replay the game

--- script.js
+++ script.js
@@ -4,12 +4,50 @@ let square;
 
 let squares = [];
 
+
+function winnerConfig(num1,num2,num3){
+    title.innerHTML = `${squares[num1]} IS THE WINNER`;
+    document.getElementById("item"+num1).style.background = "#000";
+    document.getElementById("item"+num2).style.background = "#000";
+    document.getElementById("item"+num3).style.background = "#000";
+    
+    setInterval(function(){title.innerHTML += "."},1000);
+    setTimeout(function(){location.reload()},4000);
+}
+
+
+
+
 function winner(){
     //Get all squares and store them in array
-    for(let i=0 ; i<10 ; i++){
-        squares[i] = document.getElementById("item"+1);
+    for(let i=1 ; i<10 ; i++){
+        squares[i] = document.getElementById("item"+i).innerHTML;
     }
-
+    if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1]){
+        winnerConfig(1,2,3);
+    }
+    else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[4]){
+        winnerConfig(4,5,6);
+    }
+    else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[7]){
+        winnerConfig(7,8,9);
+    }
+    else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[1]){
+        winnerConfig(1,5,9);
+    }
+    else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1]){
+        winnerConfig(1,4,7);
+    }
+    else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[2]){
+        winnerConfig(2,5,8);
+    }
+    else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[3]){
+        winnerConfig(3,6,9);
+    }
+    else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[3]){
+        winnerConfig(3,5,7);
+    }
+    
 }
 
 function game(id){
@@ -23,7 +61,7 @@ function game(id){
         // move turn to "O"
         turn = "o";
         title.innerHTML = "O TURN"
-        
+        winner()
     }
 
     // check if it's O turn and the square is empty
@@ -34,6 +72,7 @@ function game(id){
         // move turn to "X"
         turn = "x";
         title.innerHTML = "X TURN"
+        winner()
     }
 
 }
